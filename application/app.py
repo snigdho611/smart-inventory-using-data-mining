d2031dc1aad26e9f08bdd043e51239c8eccf7b99
snigdho611 <61914319+snigdho611@users.noreply.github.com>
2021-05-08 17:15:30 +0600

    Adding data templates

--- application/app.py
+++ application/app.py
@@ -8,4 +8,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 app.run(host='localhost', port=5000)
